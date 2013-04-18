59a3ad625972bdece4e9cdfd702cbf6a5b206928
Johannes Schwendinger <jotschgl@gmail.com>
2013-04-18 15:39:59 +0200

    Javadoc

--- src/dk/dtu/ait/chess/at/chess/figures/Figure.java
+++ src/dk/dtu/ait/chess/at/chess/figures/Figure.java
@@ -2,23 +2,57 @@ package dk.dtu.ait.chess.at.chess.figures;
 
 import dk.dtu.ait.chess.at.chess.Move;
 
+import java.awt.*;
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA.
- * User: johannes
- * Date: 18.04.13
- * Time: 14:56
- * To change this template use File | Settings | File Templates.
+ *   This class is the base class for all the figures
+ *
+ *   It defines the main functions a figure must be able to perform
  */
 public abstract class Figure {
 
     private int position;
+    private Color color;
 
+    public Figure(int position, Color color)
+    {
+        this.color = color;
+        this.position = position;
+    }
+
+    /**
+     * Returns the position of the Figure on the Board.
+     * @return the position
+     */
     public int getPosition() {
         return position;
     }
 
+    /**
+     * Returns the color of the figure
+     * @return the color of the figure
+     */
+    public Color color()
+    {
+        return color;
+    }
+
+
+    /**
+     * This function is used to get all the Moves the figure can execute.
+     * @return a list with all the possible moves inside.
+     */
     public abstract List<Move> getMoves();
+
+
+    /**
+     * Returns the name of the figure. Not sure if ever used.
+     * @return the name of the figure
+     */
+    public String getName()
+    {
+        return this.getClass().getName().split(".")[this.getClass().getName().split(".").length-1];
+    }
 
 }
