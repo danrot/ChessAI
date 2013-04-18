fe608f506377ee95e849261caacbf682a37fdc7a
Daniel Rotter <daniel.rotter@gmail.com>
2013-04-18 15:30:40 +0200

    implemented a simple class for moves

--- src/dk/dtu/ait/chess/at/chess/Move.java
+++ src/dk/dtu/ait/chess/at/chess/Move.java
@@ -3,30 +3,87 @@ package dk.dtu.ait.chess.at.chess;
 import dk.dtu.ait.chess.at.chess.figures.Figure;
 
 /**
- * Created with IntelliJ IDEA.
- * User: johannes
- * Date: 18.04.13
- * Time: 14:55
- * To change this template use File | Settings | File Templates.
+ * This class represents a move.
+ *
+ * Therefore it saves the value of the field and the figure on this field where the moves starts ("old"-values),
+ * and the field where the move goes ("new"-values). Additional there is a special-variable, containing if there is
+ * a move like a pawn promotion or a castling.
  */
 public class Move {
+    /**
+     * The field on which the move starts
+     */
+    private int oldField;
+    /**
+     * The field on which the move ends
+     */
+    private int newField;
+    /**
+     * Indicates if this instance is a special move
+     */
+    private boolean special;
+    /**
+     * The figure standing on the starting (old) field
+     */
+    private Figure oldFigure;
+    /**
+     * The figure standing on the ending (new) field
+     */
+    private Figure newFigure;
+
+    /**
+     *
+     * @param oldField The field on which the move starts
+     * @param newField The field on which the move ends
+     * @param oldFigure The figure standing on the starting (old) field
+     * @param newFigure The figure standing on the ending (new) field
+     * @param special Indicates if this instance is a special move
+     */
+    public Move(int oldField, int newField, Figure oldFigure, Figure newFigure, boolean special) {
+        this.oldField = oldField;
+        this.newField = newField;
+        this.special = special;
+        this.oldFigure = oldFigure;
+        this.newFigure = newFigure;
+    }
+
+    /**
+     * Returns the field on which the move starts
+     * @return The field on which the move starts
+     */
     public int getOldField() {
-        return 0; //TODO Implement
+        return oldField;
     }
 
+    /**
+     * Returns the field on which the move ends
+     * @return The field on which the move ends
+     */
     public int getNewField() {
-        return 0; //TODO Implement
+        return newField;
     }
 
+    /**
+     * Returns if the move is a special one
+     * @return True if the move is a special one, otherwise false
+     */
     public boolean getSpecial() {
-        return false;
+        return special;
     }
 
+    /**
+     * Returns the figure standing on the starting field
+     * @return The figure standing on the starting field
+     */
     public Figure getOldFigure() {
-        return null;
+        return oldFigure;
     }
 
+    /**
+     * Returns the figure standing on the ending field
+     * @return The figure standing on the ending field
+     */
     public Figure getNewFigure() {
-        return null;
+        return newFigure;
     }
 }
