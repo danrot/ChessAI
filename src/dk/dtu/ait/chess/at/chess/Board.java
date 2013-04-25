c684d24955a708e751652722306cacab233f5e27
Daniel Rotter <daniel.rotter@gmail.com>
2013-04-25 15:56:59 +0200

    added rule checking for king

--- src/dk/dtu/ait/chess/at/chess/Board.java
+++ src/dk/dtu/ait/chess/at/chess/Board.java
@@ -3,20 +3,68 @@ package dk.dtu.ait.chess.at.chess;
 import dk.dtu.ait.chess.at.chess.figures.Figure;
 
 /**
- * Created with IntelliJ IDEA.
- * User: daniel
- * Date: 18.04.13
- * Time: 15:16
- * To change this template use File | Settings | File Templates.
+ * Holds all the figures on the board and is responsible for any operation including the board itself.
+ * <p/>
+ * Makes use of the 0x88-Notation for a chess board (http://chessprogramming.wikispaces.com/0x88).
  */
 public class Board {
+    /**
+     * The figures on the board, represented in the 0x88-notation
+     */
     private Figure[] board = new Figure[128];
+    /**
+     * The bitmask to check if the field is on the board
+     */
+    private final int BOARD_MASK = 0x88;
+
+    public Figure getFigure(int position) {
+        return board[position];
+    }
 
     public boolean apply(Move move) {
         return false; //TODO Implement
     }
 
+    /**
+     * Checks if the given move is a legal one.
+     * <p/>
+     * Also sets the new figure in the move, as the move itself is not aware of it.
+     * However, this is not done if the move is invalid.
+     *
+     * @param move The move to check
+     * @return True if the move is valid, otherwise false
+     */
     public boolean check(Move move) {
-        return true; //TODO Implement
+        if ((move.getNewField() & BOARD_MASK) > 0) {
+            //Move is not allowed if the position is out of the board
+            return false;
+        }
+        if (board[move.getNewField()].getColor() == move.getOldFigure().getColor()) {
+            //Move is also not allowed if the new position is the same color as the old one, because it is not
+            //possible to capture its own figure
+            return false;
+        }
+
+        //These checkings differ on the figure
+        String figure = move.getOldFigure().getName();
+        int newField = move.getNewField();
+        int oldField = move.getOldField();
+        if (figure == "King") {
+            //King is only allowed to move one field
+            if (!(newField + 0x11 == oldField &&
+                    newField + 0x10 == oldField &&
+                    newField + 0x09 == oldField &&
+                    newField + 0x01 == oldField &&
+                    newField - 0x01 == oldField &&
+                    newField - 0x09 == oldField &&
+                    newField - 0x10 == oldField &&
+                    newField - 0x11 == oldField
+            )) {
+                return false;
+            }
+        }
+
+        move.setNewFigure(board[move.getNewField()]);
+        return true;
     }
 }
