013825f61bcd41fb5644e2bedc47c7fba8c7c082
Johannes Schwendinger <jotschgl@gmail.com>
2013-05-13 13:26:03 +0200

    no more check in pawn, queen and rook

--- src/dk/dtu/ait/chess/at/chess/figures/Pawn.java
+++ src/dk/dtu/ait/chess/at/chess/figures/Pawn.java
@@ -34,17 +34,13 @@ public class Pawn extends Figure {
             for (int i = 0; i < whiteMoves[position].length; i++) {
                 Move m = new Move(whiteMoves[position][i]);
                 m.setOldFigure(this);
-                if (board.check(m)) {
-                    ret.add(m);
-                }
+                ret.add(m);
             }
         } else {
             for (int i = 0; i < blackMoves[position].length; i++) {
                 Move m = new Move(blackMoves[position][i]);
                 m.setOldFigure(this);
-                if (board.check(m)) {
-                    ret.add(m);
-                }
+                ret.add(m);
             }
         }
         return ret;
@@ -69,7 +65,7 @@ public class Pawn extends Figure {
                 if (i >= 0x60 && i <= 0x67) {
                     captureLeft.setSpecial(true);
                 }
-                if ((captureLeft.getNewField() & 0x10) > 0 && captureLeft.getNewField() > 0) {
+                if ((captureLeft.getNewField() & 0x88) > 0 && captureLeft.getNewField() > 0) {
 
                     retWhite.add(captureLeft);
                 }
@@ -81,7 +77,7 @@ public class Pawn extends Figure {
                 if (i >= 0x60 && i <= 0x67) {
                     captureRight.setSpecial(true);
                 }
-                if ((captureRight.getNewField() & 0x10) > 0 && captureRight.getNewField() > 0) {
+                if ((captureRight.getNewField() & 0x88) > 0 && captureRight.getNewField() > 0) {
 
                     retWhite.add(captureRight);
                 }
@@ -93,7 +89,7 @@ public class Pawn extends Figure {
                     m1.setSpecial(true);
                 }
 
-                if ((m1.getNewField() & 0x10) > 0 && m1.getNewField() > 0) {
+                if ((m1.getNewField() & 0x88) > 0 && m1.getNewField() > 0) {
 
                     retWhite.add(m1);
                 }
@@ -102,7 +98,7 @@ public class Pawn extends Figure {
                     m2.setNewField(i + 0x20);
                     m2.setOldField(i);
 
-                    if ((m2.getNewField() & 0x10) > 0 && m2.getNewField() > 0) {
+                    if ((m2.getNewField() & 0x88) > 0 && m2.getNewField() > 0) {
                         retWhite.add(m2);
                     }
                 }
@@ -116,7 +112,7 @@ public class Pawn extends Figure {
                 if (i >= 0x10 && i <= 0x17) {
                     captureLeft.setSpecial(true);
                 }
-                if ((captureLeft.getNewField() & 0x10) > 0 && captureLeft.getNewField() > 0) {
+                if ((captureLeft.getNewField() & 0x88) > 0 && captureLeft.getNewField() > 0) {
 
                     retBlack.add(captureLeft);
                 }
@@ -127,7 +123,7 @@ public class Pawn extends Figure {
                 if (i >= 0x10 && i <= 0x17) {
                     captureRight.setSpecial(true);
                 }
-                if ((captureRight.getNewField() & 0x10) > 0 && captureRight.getNewField() > 0) {
+                if ((captureRight.getNewField() & 0x88) > 0 && captureRight.getNewField() > 0) {
 
                     retBlack.add(captureRight);
                 }
@@ -140,7 +136,7 @@ public class Pawn extends Figure {
                     m1.setSpecial(true);
                 }
 
-                if ((m1.getNewField() & 0x10) > 0 && m1.getNewField() > 0) {
+                if ((m1.getNewField() & 0x88) > 0 && m1.getNewField() > 0) {
                     retBlack.add(m1);
                 }
 
@@ -150,7 +146,7 @@ public class Pawn extends Figure {
                     m2.setOldField(i);
 
 
-                    if ((m2.getNewField() & 0x10) > 0 && m2.getNewField() > 0) {
+                    if ((m2.getNewField() & 0x88) > 0 && m2.getNewField() > 0) {
                         retBlack.add(m2);
                     }
                 }
@@ -158,120 +154,6 @@ public class Pawn extends Figure {
             blackMoves[i] = retBlack.toArray(new Move[0]);
 
         }
-
-    }
-
-
-    public List<Move> getMoves_alt(Board board) {
-        List<Move> ret = new ArrayList<Move>(2);
-
-        if (getColor() == Color.white) {
-            Move captureLeft = new Move();
-            captureLeft.setNewField(position + 0x10 - 0x01);
-            captureLeft.setOldField(position);
-            captureLeft.setOldFigure(this);
-
-
-            if (position >= 0x60 && position <= 0x67) {
-                captureLeft.setSpecial(true);
-            }
-            if (board.check(captureLeft)) {
-
-                ret.add(captureLeft);
-            }
-
-            Move captureRight = new Move();
-            captureRight.setNewField(position + 0x11);
-            captureRight.setOldField(position);
-            captureRight.setOldFigure(this);
-
-
-            if (position >= 0x60 && position <= 0x67) {
-                captureRight.setSpecial(true);
-            }
-            if (board.check(captureRight)) {
-
-                ret.add(captureRight);
-            }
-
-            Move m1 = new Move();
-            m1.setNewField(position + 0x10);
-            m1.setOldField(position);
-            m1.setOldFigure(this);
-
-            if (position >= 0x60 && position <= 0x67) {
-                m1.setSpecial(true);
-            }
-
-            if (board.check(m1)) {
-
-                ret.add(m1);
-            }
-            if (!hasMoved()) {
-                Move m2 = new Move();
-                m2.setNewField(position + 0x20);
-                m2.setOldField(position);
-                m2.setOldFigure(this);
-
-                if (board.check(m2)) {
-                    ret.add(m2);
-                }
-            }
-        } else if (getColor() == Color.black) {
-
-            Move captureLeft = new Move();
-            captureLeft.setNewField(position - 0x10 - 0x01);
-            captureLeft.setOldField(position);
-            captureLeft.setOldFigure(this);
-            if (position >= 0x10 && position <= 0x17) {
-                captureLeft.setSpecial(true);
-            }
-            if (board.check(captureLeft)) {
-
-                ret.add(captureLeft);
-            }
-            Move captureRight = new Move();
-            captureRight.setNewField(position - 0x11);
-            captureRight.setOldField(position);
-            captureRight.setOldFigure(this);
-
-            if (position >= 0x10 && position <= 0x17) {
-                captureRight.setSpecial(true);
-            }
-            if (board.check(captureRight)) {
-
-                ret.add(captureRight);
-            }
-
-            Move m1 = new Move();
-            m1.setNewField(position - 0x10);
-            m1.setOldField(position);
-            m1.setOldFigure(this);
-
-            if (position >= 0x10 && position <= 0x17) {
-                m1.setSpecial(true);
-            }
-
-            if (board.check(m1)) {
-
-                ret.add(m1);
-            }
-
-            if (!hasMoved()) {
-                Move m2 = new Move();
-                m2.setNewField(position - 0x20);
-                m2.setOldField(position);
-                m2.setOldFigure(this);
-
-
-                if (board.check(m2)) {
-
-                    ret.add(m2);
-                }
-            }
-        }
-
-        return ret;
 
     }
 
--- src/dk/dtu/ait/chess/at/chess/figures/Queen.java
+++ src/dk/dtu/ait/chess/at/chess/figures/Queen.java
@@ -21,14 +21,11 @@ public class Queen extends Figure {
 
     @Override
     public List<Move> getMoves(Board board) {
-        ArrayList<Move>  ret = new ArrayList<Move>(50);
-        for (int i = 0; i < moves[position].length; i++)
-        {
+        ArrayList<Move> ret = new ArrayList<Move>(50);
+        for (int i = 0; i < moves[position].length; i++) {
             Move m = new Move(moves[position][i]);
             m.setOldFigure(this);
-            if (board.check(m)) {
-                ret.add(m);
-            }
+            ret.add(m);
         }
         return ret;
     }
@@ -76,7 +73,7 @@ public class Queen extends Figure {
                     default:
                         throw new RuntimeException();
                 }
-                if ((u.getNewField() & 0x10) > 0 && u.getNewField() > 0)
+                if ((u.getNewField() & 0x88) > 0 && u.getNewField() > 0)
                     ret.add(u);
             }
             //right
@@ -112,7 +109,7 @@ public class Queen extends Figure {
                     default:
                         throw new RuntimeException();
                 }
-                if ((r.getNewField() & 0x10) > 0 && r.getNewField() > 0)
+                if ((r.getNewField() & 0x88) > 0 && r.getNewField() > 0)
                     ret.add(r);
             }
             //down
@@ -147,7 +144,7 @@ public class Queen extends Figure {
                     default:
                         throw new RuntimeException();
                 }
-                if ((d.getNewField() & 0x10) > 0 && d.getNewField() > 0)
+                if ((d.getNewField() & 0x88) > 0 && d.getNewField() > 0)
                     ret.add(d);
 
             }
@@ -183,7 +180,7 @@ public class Queen extends Figure {
                     default:
                         throw new RuntimeException();
                 }
-                if ((l.getNewField() & 0x10) > 0 && l.getNewField() > 0)
+                if ((l.getNewField() & 0x88) > 0 && l.getNewField() > 0)
                     ret.add(l);
 
             }
@@ -296,290 +293,12 @@ public class Queen extends Figure {
                             }
                             break;
                     }
-                    if ((m.getNewField() & 0x10) > 0 && m.getNewField() > 0)
+                    if ((m.getNewField() & 0x88) > 0 && m.getNewField() > 0)
                         ret.add(m);
                 }
             }
-               moves[ii] = ret.toArray(new Move[0]) ;
+            moves[ii] = ret.toArray(new Move[0]);
         }
-    }
-
-
-    public List<Move> getMoves_alt(Board board) {
-        ArrayList<Move> ret = new ArrayList<Move>(30);
-        //up
-        for (int i = 1; i < 8; i++) {
-            Move u = new Move();
-            u.setOldField(position);
-            u.setOldFigure(this);
-            switch (i) {
-                case 1:
-                    u.setNewField(position + 0x10);
-                    break;
-                case 2:
-                    u.setNewField(position + 0x20);
-                    break;
-                case 3:
-                    u.setNewField(position + 0x30);
-                    break;
-                case 4:
-                    u.setNewField(position + 0x40);
-                    break;
-                case 5:
-                    u.setNewField(position + 0x50);
-                    break;
-                case 6:
-                    u.setNewField(position + 0x60);
-                    break;
-                case 7:
-                    u.setNewField(position + 0x70);
-                    break;
-                case 8:
-                    u.setNewField(position + 0x80);
-                    break;
-                default:
-                    throw new RuntimeException();
-            }
-            if (board.check(u))
-                ret.add(u);
-            else
-                break;
-        }
-        //right
-        for (int i = 1; i < 8; i++) {
-
-            Move r = new Move();
-            r.setOldField(position);
-            r.setOldFigure(this);
-            switch (i) {
-                case 1:
-                    r.setNewField(position + 0x01);
-                    break;
-                case 2:
-                    r.setNewField(position + 0x02);
-                    break;
-                case 3:
-                    r.setNewField(position + 0x03);
-                    break;
-                case 4:
-                    r.setNewField(position + 0x04);
-                    break;
-                case 5:
-                    r.setNewField(position + 0x05);
-                    break;
-                case 6:
-                    r.setNewField(position + 0x06);
-                    break;
-                case 7:
-                    r.setNewField(position + 0x07);
-                    break;
-                case 8:
-                    r.setNewField(position + 0x08);
-                    break;
-                default:
-                    throw new RuntimeException();
-            }
-            if (board.check(r))
-                ret.add(r);
-            else
-                break;
-        }
-        //down
-        for (int i = 1; i < 8; i++) {
-            Move d = new Move();
-            d.setOldField(position);
-            d.setOldFigure(this);
-            switch (i) {
-                case 1:
-                    d.setNewField(position - 0x10);
-                    break;
-                case 2:
-                    d.setNewField(position - 0x20);
-                    break;
-                case 3:
-                    d.setNewField(position - 0x30);
-                    break;
-                case 4:
-                    d.setNewField(position - 0x40);
-                    break;
-                case 5:
-                    d.setNewField(position - 0x50);
-                    break;
-                case 6:
-                    d.setNewField(position - 0x60);
-                    break;
-                case 7:
-                    d.setNewField(position - 0x70);
-                    break;
-                case 8:
-                    d.setNewField(position - 0x80);
-                    break;
-                default:
-                    throw new RuntimeException();
-            }
-            if (board.check(d))
-                ret.add(d);
-            else
-                break;
-        }
-        //left
-        for (int i = 1; i < 8; i++) {
-            Move l = new Move();
-            l.setOldField(position);
-            l.setOldFigure(this);
-            switch (i) {
-                case 1:
-                    l.setNewField(position - 0x01);
-                    break;
-                case 2:
-                    l.setNewField(position - 0x02);
-                    break;
-                case 3:
-                    l.setNewField(position - 0x03);
-                    break;
-                case 4:
-                    l.setNewField(position - 0x04);
-                    break;
-                case 5:
-                    l.setNewField(position - 0x05);
-                    break;
-                case 6:
-                    l.setNewField(position - 0x06);
-                    break;
-                case 7:
-                    l.setNewField(position - 0x07);
-                    break;
-                case 8:
-                    l.setNewField(position - 0x08);
-                    break;
-                default:
-                    throw new RuntimeException();
-            }
-            if (board.check(l))
-                ret.add(l);
-            else
-                break;
-        }
-
-
-        for (int I = 0; I < 4; I++) {
-            for (int i = 1; i <= 7; i++) {
-                Move m = new Move();
-                m.setOldField(position);
-                m.setOldFigure(this);
-
-                switch (I) {
-                    case 0:
-                        switch (i) {
-                            case 1:
-                                m.setNewField(position + 0x11);
-                                break;
-                            case 2:
-                                m.setNewField(position + 0x22);
-                                break;
-                            case 3:
-                                m.setNewField(position + 0x33);
-                                break;
-                            case 4:
-                                m.setNewField(position + 0x44);
-                                break;
-                            case 5:
-                                m.setNewField(position + 0x55);
-                                break;
-                            case 6:
-                                m.setNewField(position + 0x66);
-                                break;
-                            case 7:
-                                m.setNewField(position + 0x77);
-                                break;
-                        }
-                        break;
-                    case 1:
-                        switch (i) {
-                            case 1:
-                                m.setNewField(position - 0x11);
-                                break;
-                            case 2:
-                                m.setNewField(position - 0x22);
-                                break;
-                            case 3:
-                                m.setNewField(position - 0x33);
-                                break;
-                            case 4:
-                                m.setNewField(position - 0x44);
-                                break;
-                            case 5:
-                                m.setNewField(position - 0x55);
-                                break;
-                            case 6:
-                                m.setNewField(position - 0x66);
-                                break;
-                            case 7:
-                                m.setNewField(position - 0x77);
-                                break;
-                        }
-                        break;
-                    case 2:
-                        switch (i) {
-                            case 1:
-                                m.setNewField(position - 0x01 + 0x10);
-                                break;
-                            case 2:
-                                m.setNewField(position - 0x02 + 0x20);
-                                break;
-                            case 3:
-                                m.setNewField(position - 0x03 + 0x30);
-                                break;
-                            case 4:
-                                m.setNewField(position - 0x04 + 0x40);
-                                break;
-                            case 5:
-                                m.setNewField(position - 0x05 + 0x50);
-                                break;
-                            case 6:
-                                m.setNewField(position - 0x06 + 0x60);
-                                break;
-                            case 7:
-                                m.setNewField(position - 0x07 + 0x70);
-                                break;
-                        }
-                        break;
-                    case 3:
-                        switch (i) {
-                            case 1:
-                                m.setNewField(position + 0x01 - 0x10);
-                                break;
-                            case 2:
-                                m.setNewField(position + 0x02 - 0x20);
-                                break;
-                            case 3:
-                                m.setNewField(position + 0x03 - 0x30);
-                                break;
-                            case 4:
-                                m.setNewField(position + 0x04 - 0x40);
-                                break;
-                            case 5:
-                                m.setNewField(position + 0x05 - 0x50);
-                                break;
-                            case 6:
-                                m.setNewField(position + 0x06 - 0x60);
-                                break;
-                            case 7:
-                                m.setNewField(position + 0x07 - 0x70);
-                                break;
-                        }
-                        break;
-                }
-                if (board.check(m)) {
-                    ret.add(m);
-                } else {
-                    break;
-                }
-            }
-        }
-
-        return ret;
-
     }
 
     @Override
